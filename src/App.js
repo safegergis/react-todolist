0d694a77415f54627a0eb099b1924bb67115a3c0
Safe Gergis <safegerg@gmail.com>
2023-03-11 01:03:11 -0800

    completed app and implemented clear function

--- src/App.js
+++ src/App.js
@@ -3,9 +3,6 @@ import './App.css';
 import styled from "styled-components";
 import React, { useState } from 'react';
 
-
-
-
 const Container = styled.div`
   display: flex;
   align-items: center;
@@ -19,11 +16,15 @@ const Button = styled.button`
   color: white;
   height: 30px;
   width: 50px;
-  border-redius: 2px;
+  border-radius: 2px;
   cursor: pointer;
 `;
 const Text =  styled.input`
-  border: 3px solid #000;
+  border: 2px solid #000;
+  width: 200px;
+  padding: 5px;
+  border-radius: 2px;
+  margin: 5px;
 `;
 const TaskCount = styled.span`
   margin: 10px;
@@ -31,28 +32,54 @@ const TaskCount = styled.span`
 const Tasks = styled.div`
 
 `;
-const handleClick = () => {
-  const id = todoList.length + 1;
-  setTodoList((prev) => [
-    ...prev,
-    {
-      id: id,
-      task: input,
-      complete: false,
-    },
-  ]);
-  setInput("");
-};
+const LIST = styled.li`
+  listStyle:"none";
+  text-decoration: "line-through";
+`
+
 function App() {
   const [input, setInput] = useState("")
   const [todoList, setTodoList] = useState([]);
   const [completedTaskCount, setCompletedTaskCount] = useState(0);
 
+  const handleClick = () => {
+    const id = todoList.length + 1;
+    setTodoList((prev) => [
+      ...prev,
+      {
+        id: id,
+        task: input,
+        complete: false,
+      }
+    ]);
+    setInput("");
+  };
+
+const handleComplete = (id) => {
+  let list = todoList.map((task) => {
+    let item = {};
+    if (task.id == id) {
+      if (!task.complete){
+        setCompletedTaskCount(completedTaskCount+1);
+      } else {
+        setCompletedTaskCount(completedTaskCount-1);
+      }
+      item = {...task, complete: !task.complete};
+      } else item = {...task};
+      return item;
+    });
+    setTodoList(list);
+  };
+const handleClear = () => {
+  setTodoList(
+    todoList.filter(t => t.id == todoList.id)
+  )
+  }
   return (
     <Container>
         <div>
             <h2> To Do List </h2>
-            <Text value={input} onInput={(e) => setInput.apply(e.target.value)} />
+            <Text value={input} onChange={e => setInput(e.target.value)} />
             <Button onClick={() => handleClick()}>Add</Button>
           <Tasks>
             <TaskCount>
@@ -61,13 +88,27 @@ function App() {
           </Tasks>
           <div>
             <ul>
-              
+              {todoList.map((todo) => {
+                return (
+                  <LIST
+                    complete={todo.complete}
+                    id={todo.id}
+                    onClick = {() => handleComplete(todo.id)}
+                    style = {{
+                      listStyle: "none",
+                      textDecoration: todo.complete && "line-through",
+                    }}
+                    >
+                      {todo.task}
+                    </LIST>
+                );
+              })}
             </ul>
           </div>
-          <Button>Clear</Button>
+          <Button onClick={() => handleClear()}>Clear</Button>
         </div>
     </Container>
   );
-}
+};
 
 export default App;
