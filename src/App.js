50dfe7b1d15f762e67959e88169a61310f5ee56d
Safe Gergis <safegerg@gmail.com>
2023-03-10 23:54:14 -0800

    pushing new project from local repo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components";
+import React, { useState } from 'react';
 
+
+
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
+const Button = styled.button`
+  display: inline-block;
+  flex: 1;
+  border: none;
+  background-color: teal;
+  color: white;
+  height: 30px;
+  width: 50px;
+  border-redius: 2px;
+  cursor: pointer;
+`;
+const Text =  styled.input`
+  border: 3px solid #000;
+`;
+const TaskCount = styled.span`
+  margin: 10px;
+`;
+const Tasks = styled.div`
+
+`;
+const handleClick = () => {
+  const id = todoList.length + 1;
+  setTodoList((prev) => [
+    ...prev,
+    {
+      id: id,
+      task: input,
+      complete: false,
+    },
+  ]);
+  setInput("");
+};
 function App() {
+  const [input, setInput] = useState("")
+  const [todoList, setTodoList] = useState([]);
+  const [completedTaskCount, setCompletedTaskCount] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+        <div>
+            <h2> To Do List </h2>
+            <Text value={input} onInput={(e) => setInput.apply(e.target.value)} />
+            <Button onClick={() => handleClick()}>Add</Button>
+          <Tasks>
+            <TaskCount>
+              <b>Pending Tasks</b>
+            </TaskCount>
+          </Tasks>
+          <div>
+            <ul>
+              
+            </ul>
+          </div>
+          <Button>Clear</Button>
+        </div>
+    </Container>
   );
 }
 
