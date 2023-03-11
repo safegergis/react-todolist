cbf1dfde501077be75a32fc2c134f06fd397a7d8
Safe Gergis <safegerg@gmail.com>
2023-03-11 02:10:55 -0800

    new styling, does not allow empty tasks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
-import styled from "styled-components";
+import styled, {createGlobalStyle} from "styled-components";
 import React, { useState } from 'react';
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Golos Text', sans-serif;
+  }
+`;
+const Title = styled.h1`
+  font-size: 60px;
+  text-align: center;
+  color: navy;
+`;
 const Container = styled.div`
   display: flex;
   align-items: center;
   flex-direction: column;
 `;
+const BottomContainer = styled(Container)`
+  flex-directions: row;
+  justify-items: center;
+  align-items: center
+
+`;
 const Button = styled.button`
   display: inline-block;
   flex: 1;
   border: none;
-  background-color: teal;
+  background-color: navy;
   color: white;
   height: 30px;
   width: 50px;
   border-radius: 2px;
   cursor: pointer;
 `;
-const Text =  styled.input`
+const ClearButton = styled(Button)`
+  display: flex
+  align-items:center
+`;
+const Input =  styled.input`
   border: 2px solid #000;
   width: 200px;
   padding: 5px;
@@ -28,13 +47,15 @@ const Text =  styled.input`
 `;
 const TaskCount = styled.span`
   margin: 10px;
+  
 `;
 const Tasks = styled.div`
-
+  text-align: center;
 `;
 const LIST = styled.li`
   listStyle:"none";
   text-decoration: "line-through";
+  text-align: center;
 `
 
 function App() {
@@ -43,16 +64,20 @@ function App() {
   const [completedTaskCount, setCompletedTaskCount] = useState(0);
 
   const handleClick = () => {
-    const id = todoList.length + 1;
-    setTodoList((prev) => [
-      ...prev,
-      {
-        id: id,
-        task: input,
-        complete: false,
-      }
-    ]);
-    setInput("");
+    if(input == ""){
+      alert("Must input a task")
+    } else {
+      const id = todoList.length + 1;
+      setTodoList((prev) => [
+        ...prev,
+        {
+          id: id,
+          task: input,
+          complete: false,
+        }
+      ]);
+      setInput("");
+    }
   };
 
 const handleComplete = (id) => {
@@ -76,38 +101,44 @@ const handleClear = () => {
   )
   }
   return (
-    <Container>
-        <div>
-            <h2> To Do List </h2>
-            <Text value={input} onChange={e => setInput(e.target.value)} />
-            <Button onClick={() => handleClick()}>Add</Button>
-          <Tasks>
-            <TaskCount>
-              <b>Pending Tasks</b>
-            </TaskCount>
-          </Tasks>
+    <>
+    <GlobalStyle />
+      <Container>
           <div>
-            <ul>
-              {todoList.map((todo) => {
-                return (
-                  <LIST
-                    complete={todo.complete}
-                    id={todo.id}
-                    onClick = {() => handleComplete(todo.id)}
-                    style = {{
-                      listStyle: "none",
-                      textDecoration: todo.complete && "line-through",
-                    }}
-                    >
-                      {todo.task}
-                    </LIST>
-                );
-              })}
-            </ul>
+              <Title> To Do List </Title>
+              <Input value={input} onChange={e => setInput(e.target.value)} />
+              <Button onClick={() => handleClick()}>Add</Button>
+            <Tasks>
+              <TaskCount>
+                <b>Pending Tasks</b> {todoList.length}
+              </TaskCount>
+            </Tasks>
+            <div>
+              <ul>
+                {todoList.map((todo) => {
+                  return (
+                    <LIST
+                      complete={todo.complete}
+                      id={todo.id}
+                      onClick = {() => handleComplete(todo.id)}
+                      style = {{
+                        listStyle: "none",
+                        textDecoration: todo.complete && "line-through",
+                      }}
+                      >
+                        {todo.task}
+                      </LIST>
+                  );
+                })}
+              </ul>
+            </div>
+            <BottomContainer>
+              <ClearButton onClick={() => handleClear()}>Clear</ClearButton>
+              </BottomContainer>
+          
           </div>
-          <Button onClick={() => handleClear()}>Clear</Button>
-        </div>
-    </Container>
+      </Container>
+      </>
   );
 };
 
